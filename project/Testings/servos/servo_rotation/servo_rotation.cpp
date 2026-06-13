fec7e8db9d71565291f01869bc3ff485e8d4a487
NicolasGhandourUni <nicolas.ghandour@etu.unilasalle.fr>
2026-06-13 16:05:53 +0200

    Update servo_rotation.cpp

--- project/Testings/servos/servo_rotation/servo_rotation.cpp
+++ project/Testings/servos/servo_rotation/servo_rotation.cpp
@@ -1,1 +1,20 @@
-dcdc
+#include <Servo.h>
+//(z- Step) on the arduino CNC shield v3 
+#define SERVO_ROTATION_PIN 11
+Servo servo_rotation; 
+
+//angle between 0 and  90 degrees
+void rotate(int angle){
+    servo_rotation.write(int(angle*2)); 
+}
+
+void setup() {
+  servo_rotation.attach(SERVO_ROTATION_PIN);  // attaches the servo on pin 9 to the Servo object
+}
+
+void loop(){
+    for (int i = 0 ; i <= 90 ; i = i + 10){
+        rotate(i);
+        delay(500);
+    }
+}
